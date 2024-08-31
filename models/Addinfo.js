6b3e2fd1c2655905242ad68cba7e34c4472a238f
Soham Patil <patilsoham390@gmail.com>
2024-08-31 20:50:50 +0530

    first commit

---
+++ models/Addinfo.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose
+
+const AddinfoSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+    },  
+    age:{
+        type: Number,
+        required: true
+    },
+    gender:{
+        type: String,
+        required: true
+    },
+    maritalstatus:{
+        type: String,
+        required: true
+    },
+    profession:{
+        type: String,
+        required: true
+    },
+
+    date:{
+        type: Date,
+        default: Date.now
+    },
+
+
+});
+module.exports = mongoose.model('addinfo', AddinfoSchema);   
+
